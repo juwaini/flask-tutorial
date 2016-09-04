6cb542dff1478616b66cfc600e00adcc0313eec0
juwaini <juwaini@gmail.com>
2016-09-04 17:03:10 +0800

    Add 'test' route with functional test

--- functional_test.py
+++ functional_test.py
@@ -5,10 +5,14 @@ class TestFlaskPage(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.get('http://localhost:5000')
 
     def test_index_page(self):
+        self.browser.get('http://localhost:5000')
         self.assertIn('Hello, world!', self.browser.page_source)
+
+    def test_test_page(self):
+        self.browser.get('localhost:5000/test')
+        self.assertNotIn('Not Found', self.browser.page_source)
 
     def tearDown(self):
         self.browser.quit()
--- hello.py
+++ hello.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello, world!'
 
+@app.route('/test')
+def test():
+    return 'This is a \'test\' page'
+
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
